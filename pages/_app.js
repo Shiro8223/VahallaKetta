404642b23e8b39c8b332b55d84af21f95d9f7b51
Shiro8223 <16staylor21@gmail.com>
2025-08-28 23:17:58 +0100

    remove import for analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,17 @@ import '../styles/globals.css';
 import "@fontsource/inter";
 import { RefreshProvider } from '../context/RefreshContext';
 import Head from "next/head";
-import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }) {
   return (
-    <Analytics>
-      <RefreshProvider>
-        <Head>
-          <title>Vahalla Savannahs</title>
-          <link rel="icon" href="/favicon.jpg" type="image/jpg" />
-          {/* Optional: For dark/light theme icons or different sizes */}
-          {/* <link rel="icon" href="/favicon.ico" /> */}
-        </Head>
-        <Component {...pageProps} />
-      </RefreshProvider>
-    </Analytics>
+    <RefreshProvider>
+      <Head>
+        <title>Vahalla Savannahs</title>
+        <link rel="icon" href="/favicon.jpg" type="image/jpg" />
+        {/* Optional: For dark/light theme icons or different sizes */}
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <Component {...pageProps} />
+    </RefreshProvider>
   );
 }
